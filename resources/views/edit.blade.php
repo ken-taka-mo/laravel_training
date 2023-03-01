0a7ea59221be05d01bcfa6b9ab35e65fa3354c4b
ken-taka-mo <kenichi@DESKTOP-L8I0CPC>
2023-03-01 17:05:19 +0900

    郵便番号api実装

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -20,21 +20,21 @@ $prefectures = config('prefectures');
                     </div>
                     <div class="item">
                         <h3 class="item-title">会社名</h3>
-                        <div class="form-wrapper"><input type="text" name="name" value="{{old('name', $detail['name'])}}"></div>
+                        <div class="form-wrapper"><input type="text" name="name" value="{{session('form_data.name') ?? old('name', $detail['name'])}}"></div>
                     </div>
                     @error('name')
                         <p class="error">{{ $message }}</p>
                     @enderror
                     <div class="item">
                         <h3 class="item-title">担当者</h3>
-                        <div class="form-wrapper"><input type="text" name="manager_name" value="{{old('manager_name', $detail['manager_name'])}}"></div>
+                        <div class="form-wrapper"><input type="text" name="manager_name" value="{{ session('form_data.manager_name') ?? old('manager_name', $detail['manager_name'])}}"></div>
                     </div>
                     @error('manager_name')
                         <p class="error">{{ $message }}</p>
                     @enderror
                     <div class="item">
                         <h3 class="item-title">電話番号<span>(半角)</span></h3>
-                        <div class="form-wrapper"><input type="text" name="phone_number" class="tel-input" maxlength="11" value="{{old('phone_number', $detail['phone_number'])}}"></div>
+                        <div class="form-wrapper"><input type="text" name="phone_number" class="tel-input" maxlength="11" value="{{ session('form_data.phone_number') ?? old('phone_number', $detail['phone_number'])}}"></div>
                     </div>
                     @error('phone_number')
                         <p class="error">{{ $message }}</p>
@@ -44,7 +44,7 @@ $prefectures = config('prefectures');
                         <div class="address-item-wrapper">
                             <div class="address-item">
                                 <h4>郵便番号<span>(半角)</span></h4>
-                                <input type="text" name="postal_code" class="short-input" maxlength="7" value="{{old('postal_code', $detail['postal_code'])}}">
+                                <input type="text" name="postal_code" class="short-input" maxlength="7" value="{{ session('form_data.postal_code') ?? old('postal_code', $detail['postal_code'])}}">
                                 <span>(ハイフンなし)</span>
                                 <input class="btn-postal btn" type="submit" name="get_address" value="自動入力">
                             </div>
@@ -52,7 +52,7 @@ $prefectures = config('prefectures');
                                 <h4>都道府県</h4>
                                 <select name="prefecture_code">
                                 @for ($i = 1; $i <= 47; $i++)
-                                    @if (old('prefecture_code', $detail['prefecture_code']) == $i)
+                                    @if ($i == session('prefecture_code') || $i == old('prefecture_code', $detail['prefecture_code']))
                                         <option value={{$i}} selected>{{$prefectures[$i]}}</option>
                                     @else
                                         <option value={{$i}}>{{$prefectures[$i]}}</option>
@@ -62,7 +62,7 @@ $prefectures = config('prefectures');
                             </div>
                             <div class="address-item">
                                 <h4>市区町村</h4>
-                                <input type="text" name="address" value="{{old('address', $detail['address'])}}">
+                                <input type="text" name="address" value="{{ session('address') ?? old('address', $detail['address'])}}">
                             </div>
                         </div>
                     </div>
@@ -77,7 +77,7 @@ $prefectures = config('prefectures');
                     @enderror
                     <div class="item">
                         <h3 class="item-title">メールアドレス</h3>
-                        <div class="form-wrapper"><input type="text" name="mail_address" value="{{old('mail_address', $detail['mail_address'])}}"></div>
+                        <div class="form-wrapper"><input type="text" name="mail_address" value="{{ session('form_data.mail_address') ?? old('mail_address', $detail['mail_address'])}}"></div>
                     </div>
                     @error('mail_address')
                         <p class="error">{{ $message }}</p>
